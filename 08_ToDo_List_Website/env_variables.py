a97a3306a749892ddd11180bb5725701bad05b88
Artur Sniegowski <49175232+artursniegowski@users.noreply.github.com>
2022-12-29 17:24:37 +0100

    Update env_variables.py

--- 08_ToDo_List_Website/env_variables.py
+++ 08_ToDo_List_Website/env_variables.py
@@ -17,3 +17,10 @@ FLASK_SECRET_KEY = os.environ.get("FLASK_SECRET_KEY")
 # bind to enviromental variable PORT (if defined), otherwise default to 5000.
 FLASK_PORT = int(os.environ.get('PORT',5000))
 ################################################################################
+# The URI should start with postgresql:// instead of postgres://. 
+# SQLAlchemy used to accept both, but has removed support for the postgres name.
+# for the DATABASE_URL - if defined the program can use PostgresSQL database
+# if not defined the progam will use SQLite databse !
+POSTGRESQL_OR_SQLITE_DATABASE_URL = os.environ.get("DATABASE_URL", 'sqlite:///blog.db')
+if 'postgres://' in POSTGRESQL_OR_SQLITE_DATABASE_URL:
+    POSTGRESQL_OR_SQLITE_DATABASE_URL = POSTGRESQL_OR_SQLITE_DATABASE_URL.replace('postgres://', 'postgresql://')
